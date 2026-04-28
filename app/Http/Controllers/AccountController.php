0cf475b023f8146b451c762aa41fc19196467e07
Krishnil Chand <krishnil.chand@fnu.ac.fj>
2026-04-29 00:01:15 +1200

    User Authentication w/o Middleware

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -48,23 +49,44 @@ class AccountController extends Controller
         }
     }
 
-    // //This method will handle user registration form submission
-    // public function registrationSubmit(Request $request)
-    // {
-    //     // dd($request->all());
-    //     $request->validate([
-    //         'name' => 'required',
-    //         'email' => 'required|email|unique:users,email',
-    //         'password' => 'required|min:6',
-    //         'confirm_password' => 'required|same:password'
-    //     ]);
-
-    //     return redirect()->route('account.registration')->with('success', 'Registration successful!');
-    // }
-
     //This method will show user login form
     public function login()
     {
         return view('front.account.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        // Authentication logic will go here
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->passes()) {
+            if(Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                // Authentication passed...
+                return redirect()->route('account.profile');
+            } else {
+                return redirect()->route('account.login')
+                ->with('error', 'Invalid credentials. Please try again.');               
+            }
+                       
+        } else {
+            return redirect()->route('account.login')
+            ->withErrors($validator)
+            ->withInput($request->only('email')); // Redirect back with validation errors and old input
+        }
+    }
+
+    //This method will show user profile page
+    public function profile(){
+        return view('front.account.profile');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('account.login');
     }
 }
